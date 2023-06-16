cf0c3bd8d32109d8de7b61b2663978ea4e47f76a
Munana Clinton <munanaclinton@gmail.com>
2023-06-16 14:42:07 +0200

    Add styles to components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ title, clickHandler }) => <button type="button" onClick={clickHandler}>{title}</button>;
+const Button = ({ title, clickHandler, checkIt }) => <button type="button" onClick={clickHandler} className={checkIt}>{title}</button>;
 
 Button.propTypes = {
   title: PropTypes.string,
   clickHandler: PropTypes.func,
+  checkIt: PropTypes.string,
 };
 Button.defaultProps = {
   title: null,
   clickHandler: null,
+  checkIt: null,
 };
 export default Button;
--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PersonIcon from '@mui/icons-material/Person';
+import styles from '../styles/Header.module.css';
 
 const Navbar = () => (
   <header>
-    <h1>Book Store</h1>
-    <nav>
-      <ul>
-        <li><Link to="/">Book</Link></li>
-        <li><Link to="/categories">Categories</Link></li>
+    <nav className={styles.navItems}>
+      <ul className={styles.links}>
+        <li><h1 style={{ color: '#0290ff' }}>BookStore CMS</h1></li>
+        <li><Link to="/" style={{ color: '#121212' }}>BOOKS</Link></li>
+        <li><Link to="/categories" className="color-nxt">CATEGORIES</Link></li>
       </ul>
-
+      <button className={styles.user} type="button">
+        <span className={styles.icon}><PersonIcon color="primary" style={{ margin: 'auto' }} /></span>
+      </button>
     </nav>
+
   </header>
 );
 
